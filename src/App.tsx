73f84114af40dd3f62d51786d9b3ad92013dbdad
誉鑫 <454452623@qq.com>
2026-03-07 07:00:29 +0000

    fix: 路由换成HashRouter，适配手机端

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Chat from './pages/Chat';
 import Contact from './pages/Contact';
 import Errands from './pages/Errands';
